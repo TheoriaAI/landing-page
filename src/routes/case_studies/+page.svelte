c9c16f6c3af833d20c89d92ba22cc33f4593b14d
Rui Melo <rufimelo99@gmail.com>
2024-04-07 23:03:49 +0100

    updgraded case studies

--- src/routes/case_studies/+page.svelte
+++ src/routes/case_studies/+page.svelte
@@ -29,34 +29,34 @@
 	{#each data.case_studies as case_study}
 	<a href={case_study.slug}>
 	
-	<div class="p-4 max-w-sm">
-		<div class="flex rounded-lg h-full dark:bg-gray-800 bg-teal-400 p-8 flex-col  hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-			<div class="flex items-center mb-3">
-				<div
-					class="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full dark:bg-indigo-500 bg-indigo-500 text-white flex-shrink-0">
-					<svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-						stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
-						<path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
-					</svg>
+		<div class="p-4 max-w-sm">
+			<div class="card flex rounded-lg h-full p-8 flex-col">
+				<div class="flex items-center mb-3">
+					<div
+						class="w-8 h-8 mr-3 inline-flex items-center justify-center rounded-full dark:bg-indigo-500 bg-indigo-500 text-white flex-shrink-0">
+						<svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+							stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+							<path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
+						</svg>
+					</div>
+					<h2 class="text-white dark:text-white text-lg font-medium">{case_study.title}</h2>
 				</div>
-				<h2 class="text-white dark:text-white text-lg font-medium">{case_study.title}</h2>
-			</div>
-			<div class="flex flex-col justify-between flex-grow">
-				<p class="leading-relaxed text-base text-white dark:text-gray-300">
-					{case_study.description}
-				</p>
-				<p class="leading-relaxed text-base text-white dark:text-gray-300">
-					{formatDate(case_study.date)}
-				</p>
-				<!-- <a href={case_study.slug} class="mt-3 text-black dark:text-white hover:text-blue-600 inline-flex items-center">{formatDate(case_study.date)}
-					<svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-						stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
-						<path d="M5 12h14M12 5l7 7-7 7"></path>
-					</svg>
-				</a> -->
+				<div class="flex flex-col justify-between flex-grow">
+					<p class="leading-relaxed text-base text-white dark:text-gray-300">
+						{case_study.description}
+					</p>
+					<p class="leading-relaxed text-base text-white dark:text-gray-300">
+						{formatDate(case_study.date)}
+					</p>
+					<!-- <a href={case_study.slug} class="mt-3 text-black dark:text-white hover:text-blue-600 inline-flex items-center">{formatDate(case_study.date)}
+						<svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+							stroke-width="2" class="w-4 h-4 ml-2" viewBox="0 0 24 24">
+							<path d="M5 12h14M12 5l7 7-7 7"></path>
+						</svg>
+					</a> -->
+				</div>
 			</div>
 		</div>
-	</div>
 	
 	
 	</a>
@@ -67,7 +67,13 @@
 </section>
 
 <style lang="postcss">
-	p , h5, .pr-3{
+	.card {
+		background-color: var(--surface-1);
+	}
+	.card:hover {
+		background-color: var(--surface-3);
+	}
+	p , h5, h2, .pr-3{
 		color: var(--text-2);
 	}
 
